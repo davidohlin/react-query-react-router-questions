497c3e1efa8f51a9f451dce2fa906dca19d31bda
David Öhlin <hello@davidohl.in>
2023-01-28 14:19:41 +0100

    Initial commit

---
+++ src/User.tsx
@@ -0,0 +1,43 @@
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import { useLoaderData } from "react-router-dom";
+
+interface User {
+  name: string;
+}
+
+const getUser = () => {
+  // Why is this called? The data should be cached from the call in loader?
+  console.log("called!");
+  return new Promise<User>((r) => setTimeout(() => r({ name: "David" }), 1000));
+};
+
+const getUserQuery = () => ({
+  queryKey: ["user"],
+  queryFn: () => getUser(),
+});
+
+export const User = () => {
+  const { userData } = useLoaderData() as Awaited<
+    ReturnType<ReturnType<typeof loader>>
+  >;
+
+  const { data: user } = useQuery({
+    ...getUserQuery(),
+    initialData: userData,
+  });
+
+  return <div>{user?.name}</div>;
+};
+
+export const loader = (queryClient: QueryClient) => async () => {
+  const userQuery = getUserQuery();
+
+  // Why is userData possibly undefined here?
+  // `getQueryData` will return data or undefined. If so, we await `fetchQuery` which will resolve User.
+  // What am I missing?
+  const userData =
+    queryClient.getQueryData<User>(userQuery.queryKey) ??
+    (await queryClient.fetchQuery(userQuery));
+
+  return { userData };
+};
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { User, loader as userLoader } from "./User";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <User />,
+    loader: userLoader(queryClient),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
